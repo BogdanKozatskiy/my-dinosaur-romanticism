905e4f5e3e6b7cad994f18ddcf2ce0a44f9657e3
Bogdan Kozatskiy <bogdan.kozatskiy@crplab.ru>
2024-03-10 13:45:20 +0300

    Init

--- apps/my-dinosaur-romanticism/src/app/app.controller.ts
+++ apps/my-dinosaur-romanticism/src/app/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import {Post} from "./types";
 
 @Controller()
 export class AppController {
@@ -9,5 +10,12 @@ export class AppController {
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('/post')
+  async getPost(): Promise<Post> {
+    return {
+      description: 'Description'
+    };
   }
 }
